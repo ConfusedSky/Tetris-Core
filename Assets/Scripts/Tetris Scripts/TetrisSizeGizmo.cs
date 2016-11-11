82e7acbd483edfc6019f9bcaff702b1272d8f9e3
Masa Maeda <masamaedae@gmail.com>
2016-11-11 11:59:13 -0800

    Updated the TetrisSizeGizmo

--- Assets/Scripts/Tetris Scripts/TetrisSizeGizmo.cs
+++ Assets/Scripts/Tetris Scripts/TetrisSizeGizmo.cs
@@ -1,22 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(TetrisGame))]
+[RequireComponent(typeof(TetrisBoard))]
 public class TetrisSizeGizmo : MonoBehaviour
 {
 	void OnDrawGizmos()
 	{
-		TetrisGame game = gameObject.GetComponent<TetrisGame>();
+		TetrisBoard board = gameObject.GetComponent<TetrisBoard>();
 
-		float width = game.Width * game.PrefabSize.x;
-		float height = game.Height * game.PrefabSize.y;
+		float width = board.Width * board.PrefabSize.x;
+		float height = board.Height * board.PrefabSize.y;
 		Vector3 size = new Vector3( width, height, 0 );
 
-		float x = transform.position.x + game.StartingLocation.x + 
-			      width / 2 - game.PrefabSize.x / 2;
-		float y = transform.position.y + game.StartingLocation.y - 
-			      height / 2 + game.PrefabSize.y / 2;
-		float z = transform.position.z + game.StartingLocation.z;
+		float x = transform.position.x + board.StartingLocation.x + 
+			      width / 2 - board.PrefabSize.x / 2;
+		float y = transform.position.y + board.StartingLocation.y - 
+			      height / 2 + board.PrefabSize.y / 2;
+		float z = transform.position.z + board.StartingLocation.z;
 
 		Vector3 position = new Vector3( x, y, z );
 
