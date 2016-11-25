6bc02cb8fea8a28937c7896db3c85926f620aad3
Masa Maeda <masamaedae@gmail.com>
2016-11-25 15:06:21 -0800

    Added more functionality to both the MinoType and the point in preparation for the full conversion

--- Assets/Scripts/Tetris Scripts/Core Scripts/MinoType.cs
+++ Assets/Scripts/Tetris Scripts/Core Scripts/MinoType.cs
@@ -7,5 +7,28 @@ public class MinoType
 	/// Define mino represented as an array of offsets
 	// +y is down +x is right
 	public Point[] Offsets;
-	public Color Color;
+	public Color BlockColor;
+	public Point Center;
+
+	public MinoType( Point[] Offsets, Color BlockColor )
+	{
+		this.Offsets = Offsets;
+		this.BlockColor = BlockColor;
+		this.Center = Point.Origin;
+	}
+
+	public MinoType( Point[] Offsets, Color BlockColor, Point Center )
+	{
+		this.Offsets = Offsets;
+		this.BlockColor = BlockColor;
+		this.Center = Center;
+	}
+
+	public IEnumerable<Point> GetBlockOffsets( int rotation )
+	{
+		foreach( Point p in Offsets )
+		{
+			yield return p.Rotate( Center, rotation );
+		}
+	}
 }
--- Assets/Scripts/Tetris Scripts/Core Scripts/Point.cs
+++ Assets/Scripts/Tetris Scripts/Core Scripts/Point.cs
@@ -2,6 +2,8 @@
 
 public class Point
 {
+	public static readonly Point Origin = new Point( 0, 0 );
+
 	public int x = 0;
 	public int y = 0;
 
@@ -14,6 +16,43 @@ public class Point
 	public static Point operator+( Point first, Point other )
 	{
 		return new Point( first.x + other.x, first.y + other.y );
+	}
+
+	// Rotates the point around the center of rotation
+	// Rotates by 
+	// 0, 0 degrees
+	// 1, 90 degrees
+	// 2, 180 degrees
+	// 3, 270 degrees
+	public Point Rotate( Point center, int rotation )
+	{
+		Point result;
+
+		switch( rotation )
+		{
+		case 0:
+			result = new Point( x, y );
+			break;
+		case 1:
+			result = new Point( -y, x );
+			break;
+		case 2:
+			result = new Point( -x, -y );
+			break;
+		case 3:
+			result = new Point( y, -x );
+			break;
+		default:
+			result = new Point( x, y );
+			break;
+		}
+
+		return result;
+	}
+
+	public Point Rotate( int rotation )
+	{
+		return Rotate( Origin, rotation );
 	}
 }
 
