9b2695d4b48a57686e61aabfb050ec8194997623
ConfusedSky <masamaedae@gmail.com>
2017-02-06 22:46:16 -0800

    Hotfix

--- Assets/Scripts/Tetris Scripts/Core Scripts/Tetromino.cs
+++ Assets/Scripts/Tetris Scripts/Core Scripts/Tetromino.cs
@@ -5,48 +5,48 @@ public static class Tetromino
 {
 	public static readonly MinoType I = 
 		new MinoType( 
-			Offsets: new Point[]{ new Point( -4, 0 ), new Point( -2, 0 ), new Point( 0, 0 ), new Point( 2, 0 ) },
-			Color:   Color.cyan,
-			Center:  new Point( -1, 1 ),
-			Scale:   2
+			Offsets:    new Point[]{ new Point( -4, 0 ), new Point( -2, 0 ), new Point( 0, 0 ), new Point( 2, 0 ) },
+			BlockColor: Color.cyan,
+			Center:     new Point( -1, 1 ),
+			Scale:      2
 		);
 
 	public static readonly MinoType O =
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( 0, 2 ), new Point( 2, 0 ), new Point( 2, 2 ) },
-			Color:   Color.yellow,
-			Center:  new Point( 1, 1 ),
-			Scale:   2
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( 0, 2 ), new Point( 2, 0 ), new Point( 2, 2 ) },
+			BlockColor: Color.yellow,
+			Center:     new Point( 1, 1 ),
+			Scale:      2
 		);
 
 	public static readonly MinoType T = 
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( 0, -1 ), new Point( -1, 0 ), new Point( 1, 0 ) },
-			Color:   Color.magenta
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( 0, -1 ), new Point( -1, 0 ), new Point( 1, 0 ) },
+			BlockColor: Color.magenta
 		);
 
 	public static readonly MinoType J = 
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 1, 0 ), new Point( -1, -1 ) },
-			Color:   Color.blue
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 1, 0 ), new Point( -1, -1 ) },
+			BlockColor: Color.blue
 		);
 
 	public static readonly MinoType L = 
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 1, 0 ), new Point( 1, -1 ) },
-			Color:   new Color32( 255, 127, 0, 255 )
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 1, 0 ), new Point( 1, -1 ) },
+			BlockColor: new Color32( 255, 127, 0, 255 )
 		);
 
 	public static readonly MinoType S = 
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( 1, -1 ), new Point( 0, -1 ), new Point( -1, 0 ) },
-			Color:   Color.green
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( 1, -1 ), new Point( 0, -1 ), new Point( -1, 0 ) },
+			BlockColor: Color.green
 		);
 
 	public static readonly MinoType Z = 
 		new MinoType(
-			Offsets: new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 0, 1 ), new Point( 1, 1 ) },
-			Color:   Color.red
+			Offsets:    new Point[]{ new Point( 0, 0 ), new Point( -1, 0 ), new Point( 0, 1 ), new Point( 1, 1 ) },
+			BlockColor: Color.red
 		);
 
 	public static readonly MinoType[] TETROMINO_TYPES = new MinoType[]
