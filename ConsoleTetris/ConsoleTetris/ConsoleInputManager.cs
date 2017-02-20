81341d260196e0885f11f4713ff897f40c7f769f
ConfusedSky <masamaedae@gmail.com>
2017-02-20 00:16:45 -0800

    Added the console version to the main branch.

---
+++ ConsoleTetris/ConsoleTetris/ConsoleInputManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris;
+using System.Windows.Input;
+
+namespace ConsoleTetris
+{
+    class ConsoleInputManager : IInputManager
+    {
+        public ConsoleInputManager()
+        {
+
+        }
+
+        ~ConsoleInputManager()
+        {
+
+        }
+
+        private enum InputState
+        {
+            LeftDown,
+            RightDown,
+            None
+        }
+        
+        public float RepeatDelay = .1f;
+        public float RepeatRate = .045f;
+
+        private InputState state = InputState.None;
+        private float timeTillRepeat = 0;
+
+        private void KeyDownEvent( object sender, KeyEventArgs e )
+        {
+
+        }
+
+        public TetrisAction HandleInput(float deltaTime)
+        {
+            timeTillRepeat -= deltaTime;  
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if ( key.Key == ConsoleKey.Spacebar) return TetrisAction.Drop;
+                else if ( key.Key == ConsoleKey.LeftArrow)
+                {
+                    state = InputState.LeftDown;
+                    timeTillRepeat = RepeatDelay;
+                    return TetrisAction.Left;
+                }
+                else if (key.Key == ConsoleKey.RightArrow)
+                {
+                    state = InputState.RightDown;
+                    timeTillRepeat = RepeatDelay;
+                    return TetrisAction.Right;
+                }
+                else if (key.Key == ConsoleKey.DownArrow) return TetrisAction.Down;
+                else if (key.Key == ConsoleKey.C) return TetrisAction.Hold;
+                else if (key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.X) return TetrisAction.RotateRight;
+                else if (key.Key == ConsoleKey.Z) return TetrisAction.RotateLeft;
+            }
+
+            if (state == InputState.LeftDown)
+            {
+                if (!Keyboard.IsKeyDown( Key.Left ) )
+                {
+                    state = InputState.None;
+                }
+                else if (timeTillRepeat <= 0)
+                {
+                    timeTillRepeat += RepeatRate;
+                    return TetrisAction.Left;
+                }
+            }
+            else if (state == InputState.RightDown)
+            {
+                if (!Keyboard.IsKeyDown( Key.Left ))
+                {
+                    state = InputState.None;
+                }
+                else if (timeTillRepeat <= 0)
+                {
+                    timeTillRepeat += RepeatRate;
+                    return TetrisAction.Right;
+                }
+            }
+
+            return TetrisAction.None;
+        }
+    }
+}
---
+++ ConsoleTetris/ConsoleTetris/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTetris
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Tetris.RectangularTetrisBoard board = new Tetris.RectangularTetrisBoard(10, 23, 1);
+            ConsoleInputManager inputManager = new ConsoleInputManager();
+            Tetris.RandomItemGenerator<Tetris.MinoType> queue = 
+                new Tetris.RandomItemGenerator<Tetris.MinoType>( Tetris.Tetromino.TETROMINO_TYPES, 5, 4, 4 );
+            Tetris.Game game = new Tetris.Game(inputManager, board, queue);
+            RectangularTetrisBoardView view = new RectangularTetrisBoardView(board);
+
+            Console.CursorVisible = false;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while( true )
+            {
+                game.Update(((float)watch.ElapsedTicks)/Stopwatch.Frequency);
+                view.Update();
+                //while (watch.ElapsedTicks < Stopwatch.Frequency / 60) ;
+                watch.Restart();
+            }
+        }
+    }
+}
