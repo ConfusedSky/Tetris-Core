726aa89d1728bbec1c57db5a5f3df2ec81de8483
ConfusedSky <masamaedae@gmail.com>
2017-02-27 00:24:26 -0800

    Added a bunch of tests two of which don't work

--- Tetris/Tetris.Test/src/MinoTwistTest.cs
+++ Tetris/Tetris.Test/src/MinoTwistTest.cs
@@ -169,6 +169,164 @@ namespace Tetris.Test
                 mino.Rotate(Rotation.Right);
             }, () => new Point(1, board.Height - 3));
         }
+
+        [Test]
+        public void TestISpinEndRight()
+        {
+            string[] text =
+            {
+                "      ",
+                " XRX  ",
+                "XX XXX",
+                "XX    "
+            };
+            TestBase(text, Tetromino.I, () => mino.Rotate(Rotation.Right), () => new Point(4, board.Height-1 ));
+        }
+
+        [Test]
+        public void TestISpinEndLeft()
+        {
+            string[] text =
+            {
+                "       ",
+                "  X X  ",
+                "XXXLXXX",
+                "       "
+            };
+            TestBase(text, Tetromino.I, () => mino.Rotate(Rotation.Left), () => new Point(2, board.Height - 1));
+        }
+
+        [Test]
+        public void TestZSpinInPlaceRight()
+        {
+            string[] text =
+            {
+                "X   ",
+                " R X",
+                "X  X"
+            };
+            TestBase(text, Tetromino.Z, () => mino.Rotate(Rotation.Right), () => new Point(1, board.Height - 2));
+        }
+
+        [Test]
+        public void TestSSpinInPlaceLeft()
+        {
+            string[] text =
+            {
+                "   X",
+                "X L ",
+                "X  X"
+            };
+            TestBase(text, Tetromino.S, () => mino.Rotate(Rotation.Left), () => new Point(2, board.Height - 2));
+        }
+
+        [Test]
+        public void TestSSpinTuckRight()
+        {
+            string[] text =
+            {
+                "    ",
+                "R XX",
+                "X  X",
+                "  XX"
+            };
+            TestBase(text, Tetromino.S, () => mino.Rotate(Rotation.Right), () => new Point(1, board.Height-2));
+        }
+
+        [Test]
+        public void TestZSpinTuckLeft()
+        {
+            string[] text =
+            {
+                "    ",
+                "XX L",
+                "X  X",
+                "XX  "
+            };
+            TestBase(text, Tetromino.Z, () => mino.Rotate(Rotation.Left), () => new Point(2, board.Height - 2));
+        }
+
+        [Test]
+        public void TestSSpinJumpRight()
+        {
+            string[] text =
+            {
+                "XX   ",
+                "X 0  ",
+                "X XXX",
+                "X  XX",
+                "XX XX"
+            };
+            TestBase(text, Tetromino.S, () => mino.Rotate(Rotation.Right), () => new Point(1, board.Height - 2));
+        }
+
+        [Test]
+        public void TestZSpinJumpLeft()
+        {
+            string[] text =
+            {
+                "   XX",
+                "  0 X",
+                "XXX X",
+                "XX  X",
+                "XX XX"
+            };
+            TestBase(text, Tetromino.Z, () => mino.Rotate(Rotation.Left), () => new Point(3, board.Height - 2));
+        }
+
+        [Test]
+        public void TestZSpinTuckRightNoOverhang()
+        {
+            string[] text =
+            {
+                "    ",
+                "XR X",
+                "X  X",
+                "XX  "
+            };
+            TestBase(text, Tetromino.Z, () => mino.Rotate(Rotation.Right), () => new Point(2, board.Height - 2));
+        }
+
+        [Test]
+        public void TestSSpinTuckLeftNoOverhang()
+        {
+            string[] text =
+            {
+                "    ",
+                "X LX",
+                "X  X",
+                "  XX"
+            };
+            TestBase(text, Tetromino.S, () => mino.Rotate(Rotation.Left), () => new Point(1, board.Height - 2));
+        }
+
+        [Test]
+        public void TestSSpinJumpRightNoOverhang()
+        {
+            string[] text =
+            {
+                "    X",
+                "  0XX",
+                "X XXX",
+                "X  XX",
+                "XX XX"
+            };
+            TestBase(text, Tetromino.S, () => mino.Rotate(Rotation.Right), () => new Point(1, board.Height - 2));
+        }
+
+        [Test]
+        public void TestZSpinJumpLeftNoOverhang()
+        {
+            string[] text =
+            {
+                "X    ",
+                "XX0  ",
+                "XXX X",
+                "XX  X",
+                "XX XX"
+            };
+            TestBase(text, Tetromino.Z, () => mino.Rotate(Rotation.Left), () => new Point(3, board.Height - 2));
+        }
         #endregion
     }
 }
